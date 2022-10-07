6ee255f1cff4d6a3401c0a394324c5a1781f6723
yeshwanthpandeti <72211417+yeshwanthpandeti@users.noreply.github.com>
2022-10-08 01:01:56 +0530

    Update HellowWorld.java

--- HellowWorld.java
+++ HellowWorld.java
@@ -1,6 +1,5 @@
 // Your First Program
-
-public class HelloWorld {
+class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World Man!"); 
         System.out.println("Hello, World Man!"); 
